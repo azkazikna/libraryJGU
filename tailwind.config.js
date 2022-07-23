dddb73880781b0faa086e3d32643ebdb5cdac375
Azkazikna <azkazikna.aal@gmail.com>
2022-07-23 18:26:12 +0700

    add login register

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'register.html', 'login.html'],
   theme: {
     extend: {
       colors: {
