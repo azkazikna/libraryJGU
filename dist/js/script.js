72b3fda2fcc559b5f0a281c54bf7788af4c51769
Azkazikna <azkazikna.aal@gmail.com>
2022-07-23 19:23:54 +0700

    add transition

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,32 @@
+// transition
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 1.2,
+        width: "100%",
+        left: "0%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".loading-screen", {
+        duration: 1,
+        width: "100%",
+        left: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    tl.set(".loading-screen", { left: "-100%" });
+}
+
 // navbar fixed
 window.onscroll = function() {
     const header = document.querySelector('header');
---
+++ dist/js/transition.js
@@ -0,0 +1,73 @@
+// tell Barba to use the prefetch module
+barba.use(barbaPrefetch);
+// Init Barba
+barba.init({
+    cacheIgnore: false,
+    prefetchIgnore: false,
+    timeout: 30000,
+    transitions: [
+        {
+            // sync: true,
+            // css classes will look like `.fade-xxx-[-xxx]`
+            // name: 'fade',
+            before({ current, next, trigger }) {
+            },
+            async leave({ current, next, trigger }) {
+                const done = this.async();
+                pageTransition();
+                await delay(1000);
+                done();
+            },
+            afterLeave({ current, next, trigger }) {
+            },
+            beforeEnter({ current, next, trigger }) {
+            },
+            enter({ current, next, trigger }) {
+                // Slider Categories
+                const swiper1 = new Swiper('.swiper1', {
+                    // Optional parameters
+                    direction: 'horizontal',
+                    loop: true,
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+
+                    breakpoints: {
+                        // when window width is >= 640px
+                        640: {
+                        slidesPerView: 4,
+                        spaceBetween: 40
+                        },
+
+                        1080: {
+                        slidesPerView: 6,
+                        spaceBetween: 40
+                        }
+                    }
+                });
+
+                // Slider Categories
+                const swiper2 = new Swiper('.swiper2', {
+                    // Optional parameters
+                    direction: 'horizontal',
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+
+                    breakpoints: {
+                        // when window width is >= 640px
+                        640: {
+                        slidesPerView: 3,
+                        spaceBetween: 40
+                        },
+
+                        1080: {
+                        slidesPerView: 5,
+                        spaceBetween: 40
+                        }
+                    }
+                });
+            },
+			after({ current, next, trigger }) {
+            },
+        }
+    ]
+});
